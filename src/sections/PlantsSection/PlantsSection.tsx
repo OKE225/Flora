6a197471d50c3818bee2fdfe08d40c4130bb1ee5
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-02-07 20:28:56 +0100

    Implement AnimatedBackground in PlantsSection

--- src/sections/PlantsSection/PlantsSection.tsx
+++ src/sections/PlantsSection/PlantsSection.tsx
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
-import { Box } from "@mui/material";
 import "./PlantsSection.scss";
+import { Box } from "@mui/material";
+import { AnimatedBackground } from "../../components/AnimatedBackground/AnimatedBackground.tsx";
 
 export class PlantsSection extends Component {
   render() {
-    return <Box className="plants-section"></Box>;
+    return <AnimatedBackground colors={{
+      baseColor: "#41B619",
+      midtoneColor: "#00DC7D",
+      lowlightColor: "#117243",
+      highlightColor: "#4D8802",
+    }}>
+      <Box className="plants-section"></Box>
+    </AnimatedBackground>;
   }
 }

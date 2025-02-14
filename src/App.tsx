cac0e4245f367646c5d611bf637b4b9028076b1f
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-02-14 20:17:15 +0100

    Implement other sections components and set colors

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,10 @@ export const App = () => {
     <>
       <Navigation />
       <PlantsSection />
-      <FlowersSection />
+      <FlowersSection baseColor="#922D25" midtoneColor="#E20338" lowlightColor="#EE3D48" highlightColor="#B40A1B" />
+      <FlowersSection baseColor="#002D6D" midtoneColor="#0043A4" lowlightColor="#01142F" highlightColor="#0351C1" />
+      <FlowersSection baseColor="#b89900" midtoneColor="#c7b40f" lowlightColor="#d3cc00" highlightColor="#bbb205" />
+      <FlowersSection baseColor="#BCBEC0" midtoneColor="#E6E7E8" lowlightColor="#D1D3D4" highlightColor="#939598" />
     </>
   );
 };

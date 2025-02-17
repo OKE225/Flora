9abd575b368a461cefb6553f359588d63f0b8752
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-02-17 20:29:05 +0100

    Create TeamSection component

---
+++ src/sections/TeamSection/TeamSection.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { AnimatedBackground } from "../../components/AnimatedBackground/AnimatedBackground.tsx";
+
+type Props = {
+  baseColor: string;
+  midtoneColor: string;
+  lowlightColor: string;
+  highlightColor: string;
+};
+
+export const TeamSection: React.FC<Props> = ({
+  baseColor,
+  midtoneColor,
+  lowlightColor,
+  highlightColor,
+}) => {
+  return (
+    <AnimatedBackground
+      colors={{
+        baseColor: baseColor,
+        midtoneColor: midtoneColor,
+        lowlightColor: lowlightColor,
+        highlightColor: highlightColor,
+      }}></AnimatedBackground>
+  );
+};

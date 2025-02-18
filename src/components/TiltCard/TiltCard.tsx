7de22cf154b58ff97e1c7cdc3e30d7d25b58764e
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-02-18 23:07:03 +0100

    Add prop to control card border color

--- src/components/TiltCard/TiltCard.tsx
+++ src/components/TiltCard/TiltCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Tilt } from "react-tilt";
-import { Card, CardMedia, CardContent, Typography } from "@mui/material";
+import { Card, CardMedia, CardContent, Typography, Box } from "@mui/material";
 
 const tiltDefaultOptions = {
   reverse: false,
@@ -16,6 +16,7 @@ const tiltDefaultOptions = {
 
 type Props = {
   cardColor: string;
+  borderColor: string;
   image: string;
   title: string;
   desc: string;
@@ -23,24 +24,29 @@ type Props = {
 
 export const TiltCard: React.FC<Props> = (props) => {
   return (
-    <Tilt options={tiltDefaultOptions}>
-      <Card
-        sx={{
-          width: "350px",
-          background: `${props.cardColor}`,
-          color: "white",
-          boxShadow: "5px 5px 0px 0px rgb(116, 207, 70)",
-        }}>
-        <CardMedia image={props.image} sx={{ height: 250 }} />
-        <CardContent>
-          <Typography variant="h5" gutterBottom>
-            {props.title}
-          </Typography>
-          <Typography variant="body1" sx={{ color: "#ececec" }}>
-            {props.desc}
-          </Typography>
-        </CardContent>
-      </Card>
-    </Tilt>
+    <Box
+      className="card"
+      sx={{
+        width: "350px",
+      }}>
+      <Tilt options={tiltDefaultOptions}>
+        <Card
+          sx={{
+            background: `${props.cardColor}`,
+            color: "white",
+            boxShadow: `0px 0px 20px -5px ${props.borderColor}`,
+          }}>
+          <CardMedia image={props.image} sx={{ height: 250 }} />
+          <CardContent>
+            <Typography variant="h5" gutterBottom>
+              {props.title}
+            </Typography>
+            <Typography variant="body1" sx={{ color: "#ececec" }}>
+              {props.desc}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Tilt>
+    </Box>
   );
 };

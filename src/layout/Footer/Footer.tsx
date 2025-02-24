eb919261e17d8acf378a47c9d909b4d85abf93f8
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-02-24 19:41:42 +0100

    Update styles for Footer component

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -7,23 +7,81 @@ import { SiMonkeytype } from "react-icons/si";
 import { FaCodepen } from "react-icons/fa";
 
 export const Footer = () => {
-  return <footer>
-    <Box sx={{width: "70%", display: "flex", justifyContent: "space-between", alignContent: "center", alignItems: "center"}} color="white">
-        <Typography variant="h6" ><span className="footer-subtext">Copyright © 2025</span> oke_225 </Typography>
-        <Box className="icons" sx={{display: "flex", alignItems: "center"}}>
-            <Link href="https://discord.com/users/676854872678858771" color="inherit" underline="none" sx={{display: "flex", alignItems: "center", mr: 3, fontSize: 26}}>
-                <FaDiscord/>
-            </Link>
-            <Link href="https://github.com/OKE225" color="inherit" underline="none" sx={{display: "flex", alignItems: "center", mr: 3, fontSize: 26}}>
-                <TbBrandGithubFilled/>
-            </Link>
-            <Link href="https://monkeytype.com/profile/oke225" color="inherit" underline="none" sx={{display: "flex", alignItems: "center", mr: 3, fontSize: 26}}>
-                <SiMonkeytype/>
-            </Link>
-            <Link href="https://codepen.io/OKE225" color="inherit" underline="none" sx={{display: "flex", alignItems: "center", mr: 3, fontSize: 26}}>
-                <FaCodepen/>
-            </Link>
+  return (
+    <footer>
+      <Box
+        sx={{
+          width: "80%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignContent: "center",
+          alignItems: "center",
+        }}
+        color="white">
+        <Typography variant="h6">
+          <span className="footer-subtext">Copyright © 2025 by</span> oke_225
+        </Typography>
+        <Box
+          className="icons"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            color: "#adb5bd",
+          }}>
+          <Link
+            href="https://discord.com/users/676854872678858771"
+            color="inherit"
+            underline="none"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              mr: 3,
+              fontSize: 24,
+              transition: "all 0.2s ease-in-out",
+            }}>
+            <FaDiscord />
+          </Link>
+          <Link
+            href="https://github.com/OKE225"
+            color="inherit"
+            underline="none"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              mr: 3,
+              fontSize: 24,
+              transition: "all 0.2s ease-in-out",
+            }}>
+            <TbBrandGithubFilled />
+          </Link>
+          <Link
+            href="https://monkeytype.com/profile/oke225"
+            color="inherit"
+            underline="none"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              mr: 3,
+              fontSize: 24,
+              transition: "all 0.2s ease-in-out",
+            }}>
+            <SiMonkeytype />
+          </Link>
+          <Link
+            href="https://codepen.io/OKE225"
+            color="inherit"
+            underline="none"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              mr: 3,
+              fontSize: 24,
+              transition: "all 0.2s ease-in-out",
+            }}>
+            <FaCodepen />
+          </Link>
         </Box>
-    </Box>
-  </footer>;
+      </Box>
+    </footer>
+  );
 };
